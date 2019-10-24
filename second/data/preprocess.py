ef43affdebf523f3c44929a63a6d1e41345d4f88
juliendm <juliendemuelenaere@Juliens-MacBook-Pro-3.local>
2019-10-24 14:11:53 -0700

    Updates

--- second/data/preprocess.py
+++ second/data/preprocess.py
@@ -309,8 +309,9 @@ def _read_and_prep_v9(info, root_path, num_point_features, prep_func):
     # velodyne_path = str(pathlib.Path(root_path) / info['velodyne_path'])
     # velodyne_path += '_reduced'
     v_path = pathlib.Path(root_path) / info['velodyne_path']
-    v_path = v_path.parent.parent / (
-        v_path.parent.stem + "_reduced") / v_path.name
+    
+    # v_path = v_path.parent.parent / (
+    #     v_path.parent.stem + "_reduced") / v_path.name
 
 
     points = np.fromfile(
